fb9d6704165ab15af101125b65584bdd751e0f66
pijunkuan <pijunkuan@gmail.com>
2020-04-28 16:35:14 +0800

    添加cors功能

--- serve/routes/api.php
+++ serve/routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::any('version',function(){
+   echo "mini-shop v~1.0";
 });
+
