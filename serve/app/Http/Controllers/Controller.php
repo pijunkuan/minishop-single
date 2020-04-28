efe11632df8985efd2f75de2d8e4940e2135e043
pijunkuan <pijunkuan@gmail.com>
2020-04-28 15:04:08 +0800

    Update Controller.php
    回复controller

--- serve/app/Http/Controllers/Controller.php
+++ serve/app/Http/Controllers/Controller.php
@@ -11,10 +11,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function customer()
-    {
-        $user = new Customer();
-
-    }
 }
