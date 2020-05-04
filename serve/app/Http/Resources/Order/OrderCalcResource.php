7fd5167bcdf5b9a6de58136756d58f9ed734673f
pijunkuan <pijunkuan@gmail.com>
2020-05-04 22:48:41 +0800

    Update OrderCalcResource.php

--- serve/app/Http/Resources/Order/OrderCalcResource.php
+++ serve/app/Http/Resources/Order/OrderCalcResource.php
@@ -16,13 +16,14 @@ class OrderCalcResource extends JsonResource
     public function toArray($request)
     {
         $items = array();
-        foreach ($this->items as $value) {
+        foreach ($this['items'] as $value) {
             $ori_variant = ProductVariant::find($value['variant_id']);
             $img_url = null;
             if ($ori_variant) {
                 if ($img = $ori_variant->product->product_images()->orderBy('sort', 'asc')->first()) $img_url = $img->image->url;
             }
             $items[] = [
+                "variant_id"=>$value['variant_id'],
                 "product_title" => $value['product_title'],
                 "variant_title" => $value['variant_title'],
                 "price" => $value['price'],
@@ -33,12 +34,12 @@ class OrderCalcResource extends JsonResource
             ];
         }
         return [
-            'address'=>$this->address,
+            'address'=>$this['address'],
             'items'=>$items,
-            'items_amount'=>$this->items_amount,
-            'shipments_amount'=>$this->shipments_amount,
-            'discounts_amount'=>$this->discounts_amount,
-            'amount'=>$this->amount,
+            'items_amount'=>$this['items_amount'],
+            'shipments_amount'=>$this['shipments_amount'],
+            'discounts_amount'=>$this['discounts_amount'],
+            'amount'=>$this['amount'],
         ];
     }
 }
