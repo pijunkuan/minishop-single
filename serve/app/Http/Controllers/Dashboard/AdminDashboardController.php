ba3dc8fc2be42e729eae1b99476b0a71efe4f25d
pijunkuan <pijunkuan@gmail.com>
2020-05-08 17:13:03 +0800

    Update AdminDashboardController.php

--- serve/app/Http/Controllers/Dashboard/AdminDashboardController.php
+++ serve/app/Http/Controllers/Dashboard/AdminDashboardController.php
@@ -27,7 +27,7 @@ class AdminDashboardController extends Controller
                     Order::ORDER_STATUS_PARTIAL,
                     Order::ORDER_STATUS_SENT,
                     Order::ORDER_STATUS_SUCCESS
-                ])->whereBetween('created_at',$day)->sum('amount');
+                ])->whereBetween('created_at', $day)->sum('amount');
                 $yesterday_order_count = Order::whereBetween('created_at', $yesterday)->count();
                 $yesterday_order_amount = Order::whereBetween('created_at', $yesterday)->sum('amount');
                 $yesterday_order_suc_amount = Order::whereIn('status', [
@@ -59,24 +59,24 @@ class AdminDashboardController extends Controller
                 ];
                 break;
             case "image":
-                $bytes = round(Image::sum('img_bytes') / pow(1024,1), 2);
-                if($bytes > pow(1024,2)){
-                    $bytes_value = round($bytes / pow(1024,2), 2) . " GB";
-                }elseif ($bytes > 1024) {
-                    $bytes_value = round($bytes / pow(1024,1), 2) . " MB";
+                $bytes = round(Image::sum('img_bytes') / pow(1024, 1), 2);
+                if ($bytes > pow(1024, 2)) {
+                    $bytes_value = round($bytes / pow(1024, 2), 2) . " GB";
+                } elseif ($bytes > 1024) {
+                    $bytes_value = round($bytes / pow(1024, 1), 2) . " MB";
                 } else {
-                    $bytes_value = $bytes ." kB";
+                    $bytes_value = $bytes . " kB";
                 }
                 $count = Image::count();
                 $rs = [
                     "bytes" => $bytes,
                     "bytes_value" => $bytes_value,
-                    "bytes_percent" => round($bytes/(1 * pow(1024,2)) *100,3) ,
+                    "bytes_percent" => round($bytes / (1 * pow(1024, 2)) * 100, 3),
                     "count" => $count
                 ];
                 break;
             case "order_line":
-                $yesterday = Order::whereBetween('created_at',$yesterday)->whereIn('status', [
+                $yesterday = Order::whereBetween('created_at', $yesterday)->whereIn('status', [
                     Order::ORDER_STATUS_PROCESSING,
                     Order::ORDER_STATUS_PARTIAL,
                     Order::ORDER_STATUS_SENT,
@@ -85,7 +85,7 @@ class AdminDashboardController extends Controller
                     DB::raw("DATE_FORMAT(created_at, '%H' ) as name"),
                     DB::raw("SUM(amount) as value")
                 ]);
-                $today = Order::whereBetween('created_at',$day)->whereIn('status', [
+                $today = Order::whereBetween('created_at', $day)->whereIn('status', [
                     Order::ORDER_STATUS_PROCESSING,
                     Order::ORDER_STATUS_PARTIAL,
                     Order::ORDER_STATUS_SENT,
@@ -94,12 +94,12 @@ class AdminDashboardController extends Controller
                     DB::raw("DATE_FORMAT(created_at, '%H' ) as name"),
                     DB::raw("SUM(amount) as value")
                 ]);
-                $hour = date("G",time());
-                $today = $this->hourArrayMake($hour,$today);
-                $yesterday = $this->hourArrayMake(24,$yesterday);
+                $hour = date("G", time());
+                $today = $this->hourArrayMake($hour, $today);
+                $yesterday = $this->hourArrayMake(24, $yesterday);
                 $rs = [
-                  'today'=>$today,
-                  'yesterday'=>$yesterday,
+                    'today' => $this->arraySum($today),
+                    'yesterday' => $this->arraySum($yesterday),
                 ];
                 break;
             default:
@@ -112,18 +112,29 @@ class AdminDashboardController extends Controller
     private function hourArrayMake($hour, $datas)
     {
         $news = array();
-        for ($i = 0;$i <= $hour;$i++){
+        for ($i = 0; $i <= $hour; $i++) {
             $news[] = [
-                "name"=>$i,
-                "value"=>0
+                "name" => $i,
+                "value" => 0
             ];
         }
-        foreach($datas as $data){
-            foreach($news as $key=>$new){
-                if($new['name'] == $data['name']*1.00)
+        foreach ($datas as $data) {
+            foreach ($news as $key => $new) {
+                if ($new['name'] == $data['name'] * 1.00)
                     $news[$key]['value'] += $data['value'];
             }
         }
         return $news;
+    }
+
+    private function arraySum($datas)
+    {
+        $last = count($datas);
+        for ($i = 0; $i < $last; $i++) {
+            if($i!= 0){
+                $datas[$i]["value"] += $datas[$i-1]['value'];
+            }
+        }
+        return $datas;
     }
 }
