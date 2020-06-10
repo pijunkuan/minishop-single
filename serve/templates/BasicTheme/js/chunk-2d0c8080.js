cb8113a9a7e1e991576aabe9d3de56956c9e9490
chengxq <chengxq513@gmail.com>
2020-06-10 23:17:19 +0800

    update

---
+++ serve/templates/BasicTheme/js/chunk-2d0c8080.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c8080"],{"52c0":function(n,c,u){"use strict";u.r(c);var l,e,o=u("2877"),p={},s=Object(o["a"])(p,l,e,!1,null,null,null);c["default"]=s.exports}}]);
--- serve/templates/BasicTheme/pages/index.blade.php
+++ serve/templates/BasicTheme/pages/index.blade.php
@@ -1,23 +1,11 @@
 @section('css')
-
+<link href="asset('template/BasicTheme/css/app.css')" rel="stylesheet">
 @endsection
 
-<div id="app">1213123123123</div>
+<div id="app"></div>
 
 @section('js')
-<script src="asset('template/BasicTheme/asset/js/app.js')"></script>
 
+<script src="asset('template/BasicTheme/js/app.js')"></script>
 
--back
---pages
----index.blade.php
-
-<script src="asset('back/js/app.js')"></script>
-
-public/back/js/app.js
-public/back/css/app.css
-
-
-    public/template/  js/app.js
-    -> templates/   js/app.js
 @endsection
