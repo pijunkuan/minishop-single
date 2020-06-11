621def9d492c3b7ee9504d0e96a797ca06bc7c66
pijunkuan <pijunkuan@gmail.com>
2020-06-12 00:27:00 +0800

    Update RouteServiceProvider.php

--- serve/app/Providers/RouteServiceProvider.php
+++ serve/app/Providers/RouteServiceProvider.php
@@ -60,7 +60,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
              ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+             ->group(base_path('routes/front.php'));
     }
 
     /**
